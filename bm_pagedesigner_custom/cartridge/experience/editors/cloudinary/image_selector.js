fef845f4ea72af16a5f6e3239607d41d32e07085
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2019-12-22 12:05:15 +0200

    cleanup

--- bm_pagedesigner_custom/cartridge/experience/editors/cloudinary/image_selector.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var cloudinaryApi = require('~/cartridge/scripts/cloudinary/cloudinary_api');
-var Logger = require('dw/system/Logger');
-
-module.exports.init = function (editor) {
-	editor.configuration.put('cloudName', cloudinaryApi.data.getCloudName());
-	editor.configuration.put('apiKey', cloudinaryApi.data.getAPIKey());
-}
