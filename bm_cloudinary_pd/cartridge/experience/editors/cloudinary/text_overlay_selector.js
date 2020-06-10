1cc5d7a05d5e02c39d2c4ea94c5a60f10c700f52
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2020-06-10 10:37:13 +0300

    clean up

--- bm_cloudinary_pd/cartridge/experience/editors/cloudinary/text_overlay_selector.js
+++
@@ -1,8 +0,0 @@
- 'use strict';
-
-var PageMgr = require('dw/experience/PageMgr');
-var HashMap = require('dw/util/HashMap');
-
-module.exports.init = function (editor) {
-	var conf = new HashMap();
-}
