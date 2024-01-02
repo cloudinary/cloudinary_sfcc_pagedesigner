d754dead9c5c34db9ce5b3bb921c1bacd3a3bba1
Asad Saleem <asaleem@rafter.one>
2024-01-02 19:15:14 +0500

    updated the API tracking get version from version.json

--- cartridges/int_cloudinary_pd/cartridge/experience/utils/constants.js
+++ cartridges/int_cloudinary_pd/cartridge/experience/utils/constants.js
@@ -1,6 +1,5 @@
 var System = require('dw/system/System');
-
+var verJson = require('*/cartridge/scripts/cloudinary/version.json');
 module.exports.cloudinaryConstants = {
-    CLD_TRACKING_PARAM: '?_i=AH',
-    API_TRACKING_PARAM: 'CloudinarySalesForcePageDesigner/20.0.4 (CommerceCloud ' + System.compatibilityMode + ') Architecture SFCC'
+    API_TRACKING_PARAM: 'CloudinarySalesForcePageDesigner/' + verJson.version + ' (CommerceCloud ' + System.compatibilityMode + ') SFPD'
 };
