2e26f79a3328a123714f12e17ffa311076a38b1c
Asad Saleem <asaleem@rafter.one>
2025-01-21 16:32:31 +0500

    changes requested

--- cartridges/int_cloudinary_pd/cartridge/static/default/js/cloudinaryVideos.js
+++ cartridges/int_cloudinary_pd/cartridge/static/default/js/cloudinaryVideos.js
@@ -1,34 +1,6 @@
 'use strict';
 
 window.initializeCloudinaryPlayers = function () {
-    window.cld = window.cloudinary.Cloudinary.new(window.conf);
-    const convertToSnakeCase = obj => JSON.parse(JSON.stringify(obj).replace(/"([^"]+)":/g, (_, p1) => `"${p1.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase()}":`));
-    window.players.forEach(player => {
-        if (player) {
-            const pCnf = JSON.parse(player.playerConf);
-            const p = window.cld.videoPlayer(player.id, pCnf.playerConfig);
-            pCnf.sourceConfig['transformation'] = convertToSnakeCase(pCnf.sourceConfig.transformation)
-            pCnf.playerConfig['cloudName'] = pCnf.cloudName;
-            p.source(pCnf.publicId, pCnf.sourceConfig);
-            p.on('error', function (e) {
-                const error = e.Player.videojs.error();
-                if (error && error.code === 10) {
-                    p.videojs.error(null);
-                    p.videojs.error({ code: null, message: "Generating the video, please wait." });
-                }
-            })
-        }
-    });
-}
-
-
-$(document).ready(function () {
-    /**
-     * Page Designer does not become active until document.ready. Because if this
-     * it waits until streaming videos fully load. We need to delay initializing
-     * Cloudinary video player a little bit.
-     */
-
     if (!document.cloudinaryInit) {
         document.cloudinaryInit = true;
         let conf = {
@@ -38,7 +10,24 @@ $(document).ready(function () {
             conf.secure_distribution = window.cname;
             conf.private_cdn = true;
         }
-        window.conf = conf;
-        window.initializeCloudinaryPlayers();
+        window.cld = window.cldPDVideoPlayer.Cloudinary.new(conf);
+        const convertToSnakeCase = obj => JSON.parse(JSON.stringify(obj).replace(/"([^"]+)":/g, (_, p1) => `"${p1.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase()}":`));
+    
+        window.players.forEach(player => {
+            if (player) {
+                const pCnf = JSON.parse(player.playerConf);
+                const p = window.cld.videoPlayer(player.id, pCnf.playerConfig);
+                pCnf.sourceConfig['transformation'] = convertToSnakeCase(pCnf.sourceConfig.transformation)
+                pCnf.playerConfig['cloudName'] = pCnf.cloudName;
+                p.source(pCnf.publicId, pCnf.sourceConfig);
+                p.on('error', function (e) {
+                    const error = e.Player.videojs.error();
+                    if (error && error.code === 10) {
+                        p.videojs.error(null);
+                        p.videojs.error({ code: null, message: "Generating the video, please wait." });
+                    }
+                })
+            }
+        });
     }
-});
+}
